31fbdde8b69ae52d910dd81dd0f019746fb27348
Lukyanenko Yaroslav <l.yaruk1993@gmail.com>
2016-12-08 17:52:27 +0200

    m

--- js/util.js
+++ js/util.js
@@ -1,29 +1,38 @@
 var addtext = function(){
     var fontSelect = document.getElementById("font-select");
     var fontSize;
+    var fontColor;
     if(document.getElementById('size-input').value > 0)
         fontSize = document.getElementById('size-input').value;
     else fontSize = 35;
+    if(document.getElementById('color-input').value != '')
+        fontColor = document.getElementById('size-input').value;
+    else fontColor = 'red';
 
     var text = new fabric.Text(document.getElementById('text-input').value, {
         left: 100,
         top: 100,
-        fontFamily: fontSelect.options[fontSelect.selectedIndex].text,
-        fontSize: fontSize
+        fontFamily: fontSelect.options[fontSelect.selectedIndex].value,
+        fontSize: fontSize,
+        fill: fontColor
     });
     canvas.add(text);
+    canvas.renderAll();
     clearInputs();
 };
 
 var edittext = function(){
     if(canvas.getActiveObject() != null){
+        var fontSelect = document.getElementById("font-select");
         canvas.getActiveObject().text = document.getElementById('text-input').value;
+        canvas.getActiveObject().fontFamily = fontSelect.options[fontSelect.selectedIndex].value;
         canvas.getActiveObject().fontSize = document.getElementById('size-input').value;
-        canvas.renderAll();
+        canvas.getActiveObject().fill = document.getElementById('color-input').value;
     }
+    canvas.renderAll();
 };
 
-var doDelete = function (event) {
+var deletetext = function (event) {
     if (event.keyCode === 46){
         if(canvas.getActiveObject() != null){
             canvas.remove(canvas.getActiveObject());
@@ -34,6 +43,7 @@ var doDelete = function (event) {
 };
 
 var clearInputs = function () {
-    document.getElementById('text-input').value = '';
-    document.getElementById('size-input').value = '';
+    for(var i = 0;i < document.getElementsByTagName('input').length; i++) {
+        document.getElementsByTagName('input')[i].value = '';
+    }
 };
