0452601b49b1f559462afac55f87ea6f584a6c6d
Lukyanenko Yaroslav <l.yaruk1993@gmail.com>
2016-12-09 14:46:37 +0200

    m

--- js/util.js
+++ js/util.js
@@ -1,20 +1,15 @@
 var addtext = function(){
     var fontSelect = document.getElementById("font-select");
-    var fontSize;
-    var fontColor;
-    if(document.getElementById('size-input').value > 0)
-        fontSize = document.getElementById('size-input').value;
-    else fontSize = 35;
-    if(document.getElementById('color-input').value != '')
-        fontColor = document.getElementById('size-input').value;
-    else fontColor = 'red';
+    var colorSelect = document.getElementById("color-select");
+    var fontSize = (document.getElementById('size-input').value > 0) ?
+        document.getElementById('size-input').value : 35;
 
     var text = new fabric.Text(document.getElementById('text-input').value, {
         left: 100,
         top: 100,
         fontFamily: fontSelect.options[fontSelect.selectedIndex].value,
         fontSize: fontSize,
-        fill: fontColor
+        fill: colorSelect.options[colorSelect.selectedIndex].value
     });
     canvas.add(text);
     canvas.renderAll();
@@ -24,10 +19,11 @@ var addtext = function(){
 var edittext = function(){
     if(canvas.getActiveObject() != null){
         var fontSelect = document.getElementById("font-select");
+        var colorSelect = document.getElementById("color-select");
         canvas.getActiveObject().text = document.getElementById('text-input').value;
         canvas.getActiveObject().fontFamily = fontSelect.options[fontSelect.selectedIndex].value;
         canvas.getActiveObject().fontSize = document.getElementById('size-input').value;
-        canvas.getActiveObject().fill = document.getElementById('color-input').value;
+        canvas.getActiveObject().fill = colorSelect.options[colorSelect.selectedIndex].value;
     }
     canvas.renderAll();
 };
@@ -47,3 +43,38 @@ var clearInputs = function () {
         document.getElementsByTagName('input')[i].value = '';
     }
 };
+
+var colorHash = {
+    'red': 0,
+    'blue': 1,
+    'green': 2,
+    'yellow': 3
+};
+
+var fontHash = {
+    'Bungee': 0,
+    'Raleway': 1,
+    'Courier New': 2,
+    'Impact': 3
+};
+
+document.addEventListener("keydown", deletetext, false);
+document.addEventListener("DOMContentLoaded", function () {
+    var colorSelect = document.getElementById("color-select");
+    var colorOptions = Object.keys(colorHash);
+    for(var i in colorOptions){
+        var el = document.createElement("option");
+        el.textContent = colorOptions[i];
+        el.value = colorOptions[i];
+        colorSelect.appendChild(el);
+    }
+
+    var fontSelect = document.getElementById("font-select");
+    var fontOptions = Object.keys(fontHash);
+    for(var j in fontOptions){
+        el = document.createElement("option");
+        el.textContent = fontOptions[j];
+        el.value = fontOptions[j];
+        fontSelect.appendChild(el);
+    }
+});
